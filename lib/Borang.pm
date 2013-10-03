12b00bdeb645be53f7ed0e0b9da134f162be149d
Steven Haryanto <stevenharyanto@gmail.com>
2013-10-03 12:58:40 +0000

    Import of SHARYANTO/Borang-0.00 from CPAN.
    gitpan-cpan-distribution: Borang
    gitpan-cpan-version:      0.00
    gitpan-cpan-path:         SHARYANTO/Borang-0.00.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Borang.pm
@@ -0,0 +1,44 @@
+package Borang;
+
+use 5.010001;
+
+our $VERSION = '0.00'; # VERSION
+
+1;
+# ABSTRACT: Yet another form handling module
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Borang - Yet another form handling module
+
+=head1 VERSION
+
+version 0.00
+
+=head1 DESCRIPTION
+
+NOT YET IMPLEMENTED.
+
+=head1 FAQ
+
+=head2 Why the name?
+
+Borang means form in Indonesian and is currently seldom used in daily
+conversations.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
