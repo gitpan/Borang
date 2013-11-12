7a341cbbbc1a5ea116d85f4bb991287e4e2c20b2
Steven Haryanto <stevenharyanto@gmail.com>
2013-11-12 08:33:13 +0000

    Import of SHARYANTO/Borang-0.01 from CPAN.
    gitpan-cpan-distribution: Borang
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Borang-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Borang.pm
+++ lib/Borang.pm
@@ -2,7 +2,7 @@ package Borang;
 
 use 5.010001;
 
-our $VERSION = '0.00'; # VERSION
+our $VERSION = '0.01'; # VERSION
 
 1;
 # ABSTRACT: Yet another form handling module
@@ -11,17 +11,24 @@ __END__
 
 =pod
 
+=encoding UTF-8
+
 =head1 NAME
 
 Borang - Yet another form handling module
 
 =head1 VERSION
 
-version 0.00
+version 0.01
 
 =head1 DESCRIPTION
 
 NOT YET IMPLEMENTED.
+
+=head1 FUNCTIONS
+
+
+None are exported by default, but they are exportable.
 
 =head1 FAQ
 
@@ -29,6 +36,23 @@ NOT YET IMPLEMENTED.
 
 Borang means form in Indonesian and is currently seldom used in daily
 conversations.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Borang>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-ReForm>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=Borang>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
